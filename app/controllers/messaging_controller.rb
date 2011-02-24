24820c5f91489473cc985bce6b7303f3f6b4de9a
Greg Stewart <gregs@tcias.co.uk>
2011-02-24 22:45:21 +0000

    added voice support

--- app/controllers/messaging_controller.rb
+++ app/controllers/messaging_controller.rb
@@ -6,28 +6,51 @@ class MessagingController < ApplicationController
       from = params["session"]["from"]
       network = from["network"]
       from_id = from["id"] # this field contains IM login or phone number in case of incoming SMS
+
       if network == "SMS" || network == "JABBER"
         render :json => parse(initial_text)
       else if network == "VOICE"
-        render :json => Tropo::Generator.say("Hello")
+        tropo = Tropo::Generator.new
+
+        tropo.ask :name => 'zip', :bargein => true, :timeout => 60, :attempts => 2,
+                    :say => [{:event => "timeout", :value => "Sorry, I did not hear anything."},
+                   {:event => "nomatch:1 nomatch:2", :value => "Oops, that wasn't a five-digit zip code."},
+                   {:value => "Please enter your zip code to search for volunteer opportunities in your area."}],
+                    :choices => { :value => "[5 DIGITS]"}
+
+        # Add a 'hangup' to the JSON response and set which resource to go to if a Hangup event occurs on Tropo
+        tropo.on :event => 'hangup', :next => 'hangup'
+        # Add an 'on' to the JSON response and set which resource to go when the 'ask' is done executing
+        tropo.on :event => 'continue', :next => 'process_zip'
+
+        render :json => tropo.response
       else
         render :json => Tropo::Generator.say("Unsupported operation")
       end
+
     else
       render :json => {:error => "There was an error"}
     end
   end
 
+  def hangup
+    render :json => Tropo::Generator.puts " Call complete "
+  end
+
+  def process_zip
+    render :json => Tropo::Generator.puts " Process zip "
+  end
+
   private
 
-  def parse(input)
-    input.strip!
-    # do whatever parsing you need.  in this example, if user types "n what a new day", tropo will
-    # respond him with "you said: what a new day"
-    if m = input.match(/^(n|N)\s+/)
-      Tropo::Generator.say "you said: " + m.post_match
-    else
-      Tropo::Generator.say "Unsupported operation."
+    def parse(input)
+      input.strip!
+      # do whatever parsing you need.  in this example, if user types "n what a new day", tropo will
+      # respond him with "you said: what a new day"
+      if m = input.match(/^(n|N)\s+/)
+        Tropo::Generator.say "you said: " + m.post_match
+      else
+        Tropo::Generator.say "Unsupported operation."
+      end
     end
-  end
 end
